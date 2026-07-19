21eb5ac48e09af9b297b4a871e3734ef4f445485
Azamdilawar <azamdilawar786@gmail.com>
2026-07-19 15:36:19 +0500

    Enabled pgVector.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_01_212745) do
+ActiveRecord::Schema[8.0].define(version: 2026_07_19_063938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "vector"
 
   create_table "conversations", force: :cascade do |t|
     t.string "title"
